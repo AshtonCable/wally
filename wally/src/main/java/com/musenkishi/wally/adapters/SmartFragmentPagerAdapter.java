6bf1468d0fdb22c07ef7a6d80c5d6e5e5d6228b0
mahermalaeb <m.n.malaeb@gmail.com>
2017-10-05 12:04:35 +0300

    Hide tabs that are not relevant in the new design:
     - Remove "Latest", "Search" and "Random" tabs.
     - Hide their XML representation
     - Make "Saved" the second tab instead of the last
     - Rename some int constants to make the code more readable

--- wally/src/main/java/com/musenkishi/wally/adapters/SmartFragmentPagerAdapter.java
+++ wally/src/main/java/com/musenkishi/wally/adapters/SmartFragmentPagerAdapter.java
@@ -29,7 +29,12 @@ import com.musenkishi.wally.fragments.ToplistFragment;
  * Created by Musenkishi on 2014-05-26 17:26.
  */
 public class SmartFragmentPagerAdapter extends SmartFragmentStatePagerAdapter {
-    private static int NUM_ITEMS = 5;
+    private static final int TOP_LIST_FRAGMENT = 0;
+    private static final int SAVED_IMAGES_FRAGMENT = 1;
+    private static final int SEARCH_FRAGMENT = 2;
+    private static final int RANDOM_IMAGES_FRAGMENT = 3;
+    private static final int LATEST_FRAGMENT = 4;
+    private static final int NUM_ITEMS = 5;
 
     public SmartFragmentPagerAdapter(FragmentManager fragmentManager) {
         super(fragmentManager);
@@ -45,16 +50,16 @@ public class SmartFragmentPagerAdapter extends SmartFragmentStatePagerAdapter {
     @Override
     public Fragment getItem(int position) {
         switch (position) {
-            case 0:
+            case TOP_LIST_FRAGMENT:
                 return ToplistFragment.newInstance();
-            case 1:
-                return LatestFragment.newInstance();
-            case 2:
-                return SearchFragment.newInstance();
-            case 3:
-                return RandomImagesFragment.newInstance();
-            case 4:
+            case SAVED_IMAGES_FRAGMENT:
                 return SavedImagesFragment.newInstance();
+            case SEARCH_FRAGMENT:
+                return SearchFragment.newInstance();
+            case RANDOM_IMAGES_FRAGMENT:
+                return RandomImagesFragment.newInstance();
+            case LATEST_FRAGMENT:
+                return LatestFragment.newInstance();
             default:
                 return null;
         }
